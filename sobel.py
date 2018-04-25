3e9596a6d2bec44a7e9cbda7772ea499e99c3446
alfonsoirai <pon.alfonsoirai@gmail.com>
2018-04-24 22:01:08 -0500

    Sobel filter applied and gitignore

--- sobel.py
+++ sobel.py
@@ -97,25 +97,11 @@ def sobel_convertion(img):
             length = int(length)
 
             # draw the length in the edge image
-            #newpixel = img.putpixel((length,length,length))
             new_img[rownum][colnum] = rgb_to_int((length,length,length))
     misc.imsave('sobel_result.jpg', new_img)
-
-def weightedAverage(pixel):
-    return 0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2]
-
-def black_and_white(image, output_image_path):
-    grey = np.zeros((image.shape[0], image.shape[1]))
-    
-    #Recorre todos los pixeles de la imagen  
-    for rownum in range(len(image)):
-        for colnum in range(len(image[rownum])):
-            grey[rownum][colnum] = weightedAverage(image[rownum][colnum])
-    misc.imsave(output_image_path, grey)
 
 if __name__ == '__main__':
     Tk().withdraw()
     input_file = askopenfilename()
     image = misc.imread(input_file)
-    black_and_white(image,'bw_result.jpg')
     sobel_convertion(image)
